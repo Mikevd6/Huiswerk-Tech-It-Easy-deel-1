6581888b06f8d46553f8d1ae9907147a7860a368
Mikevd6 <m.vandongen@novi-education.nl>
2024-11-04 18:54:00 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { calculateSold } from './calculateSold';
-import { calculateStock } from './calculateStock';
-import { calculateRemaining } from './calculateRemaining';
+import { calculateSold } from './calculateSold.js';
+import { calculatestock } from './calculatestock.js';
+import { calculateremaining } from './calculateremaining.js';
 import inventory from './constants/inventory';
 function App() {
     return (
         <div>
             <h1>Tech It Easy Dashboard</h1>
             <p className="sold-count">Totaal verkochte TV's: {calculateSold(inventory)}</p>
-            <p className="stock-count">Totaal ingekochte TV's: {calculateStock(inventory)}</p>
-            <p className="remaining-count">Nog te verkopen TV's: {calculateRemaining(inventory)}</p>
+            <p className="stock-count">Totaal ingekochte TV's: {calculatestock(inventory)}</p>
+            <p className="remaining-count">Nog te verkopen TV's: {calculateremaining(inventory)}</p>
         </div>
     );
 }
